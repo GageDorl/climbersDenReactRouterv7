7d6c2f4a6c59da369eae1b700694f1fe559ae217
root <root@srv756825.hstgr.cloud>
2026-01-23 20:49:47 +0000

    changed stuff

--- server.ts
+++ server.ts
@@ -15,6 +15,9 @@ const viteDevServer =
 
 const app = express();
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 // Handle .well-known requests (Chrome DevTools, etc.)
 app.use((req, res, next) => {
   if (req.path.startsWith("/.well-known/")) {
